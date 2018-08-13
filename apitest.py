e36968440112b1ede70a884fbdb8475ed2ba5589
jevargas-m <jevargas_m@yahoo.com>
2018-08-13 18:47:39 -0500

    clarify examples and results from API

--- apitest.py
+++ apitest.py
@@ -1,5 +1,8 @@
 import auth
 
 print(auth.getfreq('friend'))
+print(auth.getfreq('khdkjhdf'))
 
-# Results in {'word': 'friend', 'frequency': {'zipf': 5.65, 'perMillion': 448.38, 'diversity': 0.72}}
+# Results when word is found in wordsapiv1:
+#   {'word': 'friend', 'frequency': {'zipf': 5.65, 'perMillion': 448.38, 'diversity': 0.72}}
+# Results when word is NOT found in wordsapiv1:{'success': False, 'message': 'word not found'}
