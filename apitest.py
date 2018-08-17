b67c6836b21708e06aae5c93b468b6b338265a12
jevargas-m <jevargas_m@yahoo.com>
2018-08-16 19:21:14 -0500

    new example of API usage

--- apitest.py
+++ apitest.py
@@ -2,6 +2,7 @@ import auth
 
 print(auth.getfreq('friend'))
 print(auth.getfreq('khdkjhdf'))
+print(auth.getfreq('asked'))
 
 # Results when word is found in wordsapiv1:
 #   {'word': 'friend', 'frequency': {'zipf': 5.65, 'perMillion': 448.38, 'diversity': 0.72}}
